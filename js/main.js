07f9c00822095c4932f43d737f5232071110a2be
AbiMeGab <abbybaem@gmail.com>
2022-09-25 00:42:35 -0500

    Contador y lista terminados

--- js/main.js
+++ js/main.js
@@ -1,35 +1,69 @@
 let btn = document.getElementById("Agregarbtn");
 let nom = document.getElementById("Name");
 let cant = document.getElementById("Number");
+let SpanTotal = document.getElementById("ShowTotal");
+let counter = document.getElementById("counterProducts");
+let TotalFinal = 0;
+let contador = 0;
 
-    
+
+function precio() {
+  let max = (100)
+  let min = (15)
+  return ((Math.random() * (max - min + 1) + min).toFixed(2));
+}
+
+function ValidarNombre(){
+  if(nom.value.length >=3){
+    nom.classList.remove("is-invalid");
+    nom.classList.add("is-valid");
+    return true;
+  }else{
+    nom.classList.remove("is-valid");
+    nom.classList.add("is-invalid");
+    return false;
+    //check = false;
+  }
+}
+
+function ValidarCantidad(){
+  if(cant.value.length >=1){
+    cant.classList.remove("is-invalid");
+    cant.classList.add("is-valid");
+    return true;
+  }else{
+    cant.classList.remove("is-valid");
+    cant.classList.add("is-invalid");
+    return false;
+    //check = false;
+  }
+}
 
 btn.addEventListener ("click", function (event){
     event.preventDefault();
-    
-    function precio() {         
-        let max = (100)
-        let min = (15)
-        return (Math.random() * (max - min + 1) + min);
-      }
+    if (ValidarNombre() &&ValidarCantidad()) {
+      console.log(nom.value);
+      console.log(cant.value);
+      let preciol = precio();
+      let Subtotal = (cant.value * preciol).toFixed(2);
+      TotalFinal += parseFloat(Subtotal);
+      total.innerHTML += `<tr>
+      <th scope="row" id="cantidad">${cant.value}</th>
+      <td id="product">${nom.value}</td>
+      <td id="uprice"><strong>$</strong> ${preciol}</td>
+      <td id="subt"><strong>$</strong> ${Subtotal}</td>
+      </tr>`
+      contador++;
+      counter.innerText = contador;
+      SpanTotal.innerText=" $ " + TotalFinal.toFixed(2); 
+      nom.value="";
+      cant.value="";
+      nom.classList.remove("is-invalid");
+      nom.classList.remove("is-valid");
+      cant.classList.remove("is-invalid");
+      cant.classList.remove("is-valid");
+    } else {
 
-    let numb = parseFloat(cant.value);
-    let total = (numb) * (precio);
-    console.log();
+    }
 
-    total.innerHTML += `<tr>
-    <th scope="row" id="cantidad">${cant}</th>
-    <td id="product">${nom}</td>
-    <td id="uprice">${precio}</td>
-    <td id="subt">${total}</td>
-    </tr>` 
   });
-    
-      // console.log(precio());
-
-    // res.innerHTML = resultado;
-
-// function resultado {
-//     let precio = Math.random(100)
-
-// }
